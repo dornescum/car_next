77513eaa4862aa9a3ec5fe4b7456d5d99194023d
Mihai Dornescu <dornescu@gmail.com>
2021-09-19 19:40:52 +0300

    details cartea tehnica

--- components/Detalii/Detalii.js
+++ components/Detalii/Detalii.js
@@ -2,25 +2,214 @@ import React from 'react';
 
 const Detalii = () => {
 	return (
-		<div className='container details'>
-			<div className="row">
-				<div className="col-6-lg">
-					<p className='p-1 thin-br'>test</p>
-					<p>test</p>
-					<p>test</p>
-					<p>test</p>
-					<p>test</p>
-				</div>
-				<div className="col-6-lg">
-					<p>test</p>
-					<p>test</p>
-					<p>test</p>
-					<p>test</p>
-					<p>test</p>
+		<div className='details'>
+			<div className="container">
+				<h3>Cartea tehnica</h3>
+				<div className="row gap-1">
+					<div className="col-6-lg">
+						<div className="info-details">
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Categoria</li>
+								<li>Autoturism m1</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Caroseria</li>
+								<li>AB berlina cu hayon</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Marca</li>
+								<li>Opel</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Tipul varianta</li>
+								<li> T98/DX11/Astra-G-CC</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Nr de omologare / Anul fabricatiei</li>
+								<li> ABPL123V11W77E4 / 2009</li>
+							</ul>
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Nr de identificare</li>
+								<li>WOLOTGF489G041519</li>
+							</ul>
+
+
+						</div>
+					</div>
+					<div className="col-6-lg">
+						<div className="info-details">
+
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Masele (KG)</li>
+								<li></li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Proprie</li>
+								<li>1205</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Totala maxima autorizata</li>
+								<li>1670</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Sarcina maxima utila autorizata</li>
+								<li>465</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Sarcina pe carligul de remorcare</li>
+								<li>75</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Maxima autorizata pe axe</li>
+								<li>Fata /875 </li>
+								<li>Spate /820 </li>
+							</ul>
+
+
+						</div>
+					</div>
+
+
+					<div className="col-6-lg">
+						<div className="info-details">
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Remorcabila cu dispozitiv de franare</li>
+								<li>1150</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Remorcabila cu dispozitiv de franare</li>
+								<li>570</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Nr de locuri </li>
+								<li>5</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Dimensiunile de gabarit (mm)</li>
+								<li> L 4110</li>
+								<li> I 1709</li>
+								<li> h 1425 </li>
+							</ul>
+
+						</div>
+					</div>
+					<div className="col-6-lg">
+						<div className="info-details">
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Motorul</li>
+								<li></li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Tipul</li>
+								<li>Z16XEP</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Serie</li>
+								<li>20MY2208</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Cilindree (cm3)</li>
+								<li>1598</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Putere maxima(kW)/ Turatie (min-1)</li>
+								<li>76 / 6000</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Sursa de energie</li>
+								<li>BENZINA</li>
+							</ul>
+
+
+						</div>
+					</div>
+
+					<div className="col-6-lg">
+						<div className="info-details">
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Numarul axelor</li>
+								<li>2</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Tractiune</li>
+								<li>FATA</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Dimensiunea anvelopelor</li>
+								<li></li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Fata</li>
+								<li> 195/60 R15 88 H</li>
+								<li> sau</li>
+								<li> 185/65 R15 88 H</li>
+							</ul>
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Mijloc-spate</li>
+								<li> 195/60 R15 88 H</li>
+								<li> sau</li>
+								<li> 185/65 R15 88 H</li>
+							</ul>
+
+						</div>
+					</div>
+					<div className="col-6-lg">
+						<div className="info-details">
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Zgomotul (db(A)) in mers</li>
+								<li>71</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Zgomotul (db(A)) in stationare</li>
+								<li>83</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Viteza maxima (Km/h) </li>
+								<li>188</li>
+							</ul>
+
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Capacitatea rezervorului</li>
+								<li> 52 L</li>
+							</ul>
+							<ul className="display-f justify-space-between  mb-1 mt-1 thin-br">
+								<li className="details-info">Culoarea</li>
+								<li> GRI</li>
+							</ul>
+
+						</div>
+					</div>
+
 				</div>
 
 			</div>
-			
 		</div>
 	);
 };
