6c585b3523d6faf27607a597ad80faa87691fd90
Mihai Dornescu <dornescu@gmail.com>
2021-09-19 20:13:44 +0300

    footer

--- components/UI/Footer.js
+++ components/UI/Footer.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { FaEnvelope, FaPhoneAlt} from 'react-icons/fa';
+
+
 
 const Footer = () => {
+	const [showNumber, setShowNumber] = useState(false);
+	const [showEmail, setShowEmail] = useState(false);
+
+	const handleNumber = () => {
+		setShowNumber(!showNumber)
+	}
+	const handleEmail = () => {
+		setShowEmail(!showEmail)
+	}
+
 	return (
-		<div>
-			<h1>Footer</h1>
+		<div className='footer o-70'>
+			<div className='container'>
+				<div className="row-footer mt-1">
+					<div className="col">
+						<button onClick={handleNumber} className='btn-info' >
+							<FaPhoneAlt />
+						</button>
+						<div className='row'>
+							{showNumber && <p className='show-info'>898989</p>}
+						</div>
+					</div>
+					<div className="col">
+						<button  className='btn-info'>
+							<FaEnvelope  onClick={handleEmail}/>
+						</button>
+						<div className="row">
+							{showEmail && <p className='show-info'>email@ya</p>}
+						</div>
+					</div>
+
+
+
+
+				</div>
+			</div>
 		</div>
 	);
 };
+
 
 export default Footer;
