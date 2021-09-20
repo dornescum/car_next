f509cd3e229eddc5b6e2b18371e3c59230bed5da
Mihai Dornescu <dornescu@gmail.com>
2021-09-20 22:30:49 +0300

    slider fix css

--- pages/slider/index.js
+++ pages/slider/index.js
@@ -20,125 +20,104 @@ const Index = () => {
 			<SRLWrapper>
 				<div className="container mt-3" id="second">
 					<h3>Mici imperfectiuni</h3>
-					<div className="row gap-1">
+					<div className="row">
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/30.webp">
-								<Image
+							<a href="/imp/30.webp" className='photo-imp'>
+								<img
 									src="/imp/30.webp"
 									alt="Picture of the author"
-									width={450}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
+
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/31.webp">
-								<Image
+							<a href="/imp/31.webp" className='photo-imp'>
+								<img
 									src="/imp/31.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/32.webp">
-								<Image
+							<a href="/imp/32.webp" className='photo-imp'>
+								<img
 									src="/imp/32.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/33.webp">
-								<Image
+							<a href="/imp/33.webp" className='photo-imp'>
+								<img
 									src="/imp/33.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						{/*===============*/}
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/34.webp">
-								<Image
+							<a href="/imp/34.webp" className='photo-imp'>
+								<img
 									src="/imp/34.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/35.webp">
-								<Image
+							<a href="/imp/35.webp" className='photo-imp'>
+								<img
 									src="/imp/35.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/36.webp">
-								<Image
+							<a href="/imp/36.webp" className='photo-imp'>
+								<img
 									src="/imp/36.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/37.webp">
-								<Image
+							<a href="/imp/37.webp" className='photo-imp'>
+								<img
 									src="/imp/37.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/38.webp">
-								<Image
+							<a href="/imp/38.webp" className='photo-imp'>
+								<img
 									src="/imp/38.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/39.webp">
-								<Image
+							<a href="/imp/39.webp" className='photo-imp'>
+								<img
 									src="/imp/39.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/40.webp">
-								<Image
+							<a href="/imp/40.webp" className='photo-imp'>
+								<img
 									src="/imp/40.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
@@ -146,100 +125,82 @@ const Index = () => {
 
 						{/*40===============*/}
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/41.webp">
-								<Image
+							<a href="/imp/41.webp" className='photo-imp'>
+								<img
 									src="/imp/41.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/42.webp">
-								<Image
+							<a href="/imp/42.webp" className='photo-imp'>
+								<img
 									src="/imp/42.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/43.webp">
-								<Image
+							<a href="/imp/43.webp" className='photo-imp'>
+								<img
 									src="/imp/43.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/44.webp">
-								<Image
+							<a href="/imp/44.webp" className='photo-imp'>
+								<img
 									src="/imp/44.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/45.webp">
-								<Image
+							<a href="/imp/45.webp" className='photo-imp'>
+								<img
 									src="/imp/45.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/46.webp">
-								<Image
+							<a href="/imp/46.webp" className='photo-imp'>
+								<img
 									src="/imp/46.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/47.webp">
-								<Image
+							<a href="/imp/47.webp" className='photo-imp'>
+								<img
 									src="/imp/47.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/48.webp">
-								<Image
+							<a href="/imp/48.webp" className='photo-imp'>
+								<img
 									src="/imp/48.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
 						</div>
 						<div className="col-3-lg col-6-sm col-12-xs">
-							<a href="/imp/49.webp">
-								<Image
+							<a href="/imp/49.webp" className='photo-imp'>
+								<img
 									src="/imp/49.webp"
 									alt="Picture of the author"
-									width={400}
-									height={400}
 									srl_gallery_image="true" // Add this if your thumbnail is not recognized
 								/>
 							</a>
