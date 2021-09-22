bf871fab53eeda416e9a111973696fa5256ada9c
Mihai Dornescu <dornescu@gmail.com>
2021-09-22 13:57:00 +0300

    head &&_document

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import SimpleReactLightbox from 'simple-react-lightbox'
+import '../styles/globals.css';
+import SimpleReactLightbox from 'simple-react-lightbox';
+import Head from "next/head";
+import React from "react";
 
 function MyApp({ Component, pageProps }) {
   return <SimpleReactLightbox>
+    <Head>
+      <title>Best car ever</title>
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="description" content="best car ever"/>
+      <link rel="icon" href="/car_3.ico"/>
+    </Head>
     <Component {...pageProps} />
   </SimpleReactLightbox>
 
