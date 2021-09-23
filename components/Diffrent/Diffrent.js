163d239d1c7c09d48756583cd0001676b645a34b
Mihai Dornescu <dornescu@gmail.com>
2021-09-23 19:53:49 +0300

    images fixed

--- components/Diffrent/Diffrent.js
+++ components/Diffrent/Diffrent.js
@@ -47,45 +47,55 @@ const Diffrent = () => {
 						</form>
 					</div>
 					<div className="col-6-lg col-12-sm">
-						<form action="">
-							<fieldset>
+						<div className="row">
+							<div className="col-2-lg">
+								<div className="vertical">
+								<p>test</p>
+							</div></div>
+							<div className="col-10-lg">
+								<form action="">
 
-								<div className="form-title">
-									<p>Masele</p>
-								</div>
-							</fieldset>
-							<fieldset>
-								<legend>Caroseria</legend>
-								<div className="formBox">
-									<p>AB berlina cu hayon</p>
-								</div>
-							</fieldset>
-							<fieldset>
-								<legend>Marca</legend>
-								<div className="formBox">
-									<p>Opel</p>
-								</div>
-							</fieldset>
-							<fieldset>
-								<legend>Tipul varianta</legend>
-								<div className="formBox">
-									<p>T98/DX11/Astra-G-CC</p>
-								</div>
-							</fieldset>
-							<fieldset>
-								<legend>Nr de omologare / Anul fabricatiei</legend>
-								<div className="formBox">
-									<p>ABPL123V11W77E4 / 2009</p>
-								</div>
-							</fieldset>
-							<fieldset>
-								<legend>Nr de identificare</legend>
-								<div className="formBox">
-									<p>WOLOTGF489G041519</p>
-								</div>
-							</fieldset>
+									<fieldset>
 
-						</form>
+										<div className="form-title">
+											<p>Masele</p>
+										</div>
+									</fieldset>
+									<fieldset>
+										<legend>Caroseria</legend>
+										<div className="formBox">
+											<p>AB berlina cu hayon</p>
+										</div>
+									</fieldset>
+									<fieldset>
+										<legend>Marca</legend>
+										<div className="formBox">
+											<p>Opel</p>
+										</div>
+									</fieldset>
+									<fieldset>
+										<legend>Tipul varianta</legend>
+										<div className="formBox">
+											<p>T98/DX11/Astra-G-CC</p>
+										</div>
+									</fieldset>
+									<fieldset>
+										<legend>Nr de omologare / Anul fabricatiei</legend>
+										<div className="formBox">
+											<p>ABPL123V11W77E4 / 2009</p>
+										</div>
+									</fieldset>
+									<fieldset>
+										<legend>Nr de identificare</legend>
+										<div className="formBox">
+											<p>WOLOTGF489G041519</p>
+										</div>
+									</fieldset>
+
+								</form>
+							</div>
+						</div>
+
 					</div>
 				</div>
 			</div>
